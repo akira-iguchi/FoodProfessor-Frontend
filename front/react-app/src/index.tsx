db3ee64013e3693b966fbe9ea41db6e984d5e93d
yuzuki <yuzuki.aritomo@gmail.com>
2021-07-03 03:26:37 +0900

    APIレスポンスデータをフロント画面に表示

--- front/react-app/src/index.tsx
+++ front/react-app/src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './tailwind.css'
 import App from './App'
+import ApiTest from 'ApiTest'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ApiTest />
   </React.StrictMode>,
   document.getElementById('root')
 )

2b55b6fc6b938380a16502a258161d65ab529572
akira-iguchi <akituri931@icloud.com>
2021-07-28 20:09:25 +0900

    add: url一覧ファイル追加

--- front/react-app/src/lib/api/auth.ts
+++ front/react-app/src/lib/api/auth.ts
@@ -1,21 +1,22 @@
 import client from 'lib/api/client'
 import Cookies from 'js-cookie'
+import { registerUrl, loginUrl, logOutUrl, sessionsUrl } from 'urls/index'
 
 import { RegisterParams, LoginParams } from 'types/auth'
 
 // サインアップ（新規アカウント作成）
 export const register = (params: RegisterParams) => {
-  return client.post('auth', params)
+  return client.post(registerUrl, params)
 }
 
 // サインイン（ログイン）
 export const login = (params: LoginParams) => {
-  return client.post('auth/sign_in', params)
+  return client.post(loginUrl, params)
 }
 
 // サインアウト（ログアウト）
 export const Logout = () => {
-  return client.delete('auth/sign_out', {
+  return client.delete(logOutUrl, {
     headers: {
       'access-token': Cookies.get('_access_token'),
       client: Cookies.get('_client'),
@@ -27,7 +28,7 @@ export const Logout = () => {
 // 認証済みのユーザーを取得
 export const getCurrentUser = () => {
   if (!Cookies.get('_access_token') || !Cookies.get('_client') || !Cookies.get('_uid')) return
-  return client.get('/auth/sessions', {
+  return client.get(sessionsUrl, {
     headers: {
       'access-token': Cookies.get('_access_token'),
       client: Cookies.get('_client'),
---
+++ front/react-app/src/urls/index.ts
@@ -0,0 +1,6 @@
+export const topPageUrl = '/top'
+export const registerUrl = '/auth'
+export const loginUrl = '/auth/sign_in'
+export const logOutUrl = '/auth/sign_out'
+export const sessionsUrl = '/auth/sessions'
+export const userProfileUrl = (userId: number) => `/users/${userId}`
